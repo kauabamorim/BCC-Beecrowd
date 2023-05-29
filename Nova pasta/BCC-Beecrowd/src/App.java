4eac8ffcb5081213f12b0c33dd23e043626bb24e
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-05-29 09:53:49 -0300

    2766

--- Nova pasta/BCC-Beecrowd/src/App.java
+++ Nova pasta/BCC-Beecrowd/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        String[] nomes = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            nomes[i] = scanner.next();    
+        }
+
+        System.out.println(nomes[2]);
+        System.out.println(nomes[6]);
+        System.out.println(nomes[8]);
+
+        scanner.close();
     }
 }
