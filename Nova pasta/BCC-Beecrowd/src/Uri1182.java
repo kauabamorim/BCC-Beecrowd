8a80a5242547a9ea75c4b1335408ec22dd691af2
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-05-30 08:55:12 -0300

    Uri1182 Utilizando Metodos e Matrizes

--- Nova pasta/BCC-Beecrowd/src/Uri1182.java
+++ Nova pasta/BCC-Beecrowd/src/Uri1182.java
@@ -20,26 +20,31 @@ public class Uri1182 {
         }
 
         if (T.charAt(0) == 'S') {
-            int soma = 0;
-
-            for (int i = 0; i < matriz.length; i++) {
-                soma += matriz[i][C];
-            }
+            double soma = somaColuna(matriz, C);
             System.out.println(df.format(soma));
+        } 
+        else if (T.charAt(0) == 'M') {
+            double media = calcularMediaColuna(matriz, C);
+            System.out.println(df.format(media));
         }
 
-        else 
-            if (T.charAt(0) == 'M') {
-                int soma = 0;
-
-                for (int i = 0; i < matriz.length; i++) {
-                    soma += matriz[i][C];
-                }
-                double media = (double) soma / 4;
-
-                System.out.println(df.format(media));
-            }
-
         scanner.close();
+    }
+
+    public static double somaColuna(double[][] matriz, int coluna) {
+        double soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            soma += matriz[i][coluna];
+        }
+
+        return soma;
+    }
+
+    public static double calcularMediaColuna(double[][] matriz, int coluna) {
+        double soma = somaColuna(matriz, coluna);
+        double media = soma / matriz.length;
+
+        return media;
     }
 }
